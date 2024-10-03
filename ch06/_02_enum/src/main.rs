71f0af7e08632635dd100b157164fda8e90c3cb9
BigBigBai <baiyingjian@outlook.com>
2024-10-03 15:06:20 -0400

    10/03

---
+++ ch06/_02_enum/src/main.rs
@@ -0,0 +1,23 @@
+// enum IpAddr {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
+
+// impl Message {
+//     fn call(&self) {
+//         // 方法体可以在这里定义
+//     }
+// }
+
+fn main() {
+    // let home = IpAddr::V4(127, 0, 0, 1);
+    // let loopback = IpAddr::V6(String::from("::1"));
+
+    // let m = Message::Write(String::from("hello"));
+    // m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+}
---
+++ ch06/_03_match/src/main.rs
@@ -0,0 +1,30 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    // value_in_cents(Coin::Penny);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
